844810931501fe14156dfcbb68845510b309a4e4
Andrew <andrewoliveira282@gmail>
2021-10-20 14:10:57 -0300

    aula 2

---
+++ src/aula1/Main.java
@@ -0,0 +1,28 @@
+package aula1;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        File file = new File( "c:\\teste\\oi.txt.txt");
+        Scanner sc = null;
+        try{
+            sc= new Scanner(file);
+            while(sc.hasNextLine()){
+                System.out.println(sc.nextLine());
+            }
+        }
+        catch (IOException e){
+            System.out.println("ERROR:"+ e.getMessage());
+        }
+        finally{
+            if(sc != null) {
+                sc.close();
+            }
+        }
+    }
+}
